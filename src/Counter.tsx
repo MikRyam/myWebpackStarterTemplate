ae3f4353b8e3a8d390544673cdb4253c7ac11786
MikRyam <mikhailryamov@gmail.com>
2023-02-26 00:08:28 +0300

    check husky

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -3,12 +3,14 @@ import styles from './Counter.module.scss';
 
 const Counter: FC = () => {
   const [count, setCount] = useState(0);
+  const name = 'Husky';
   return (
     <div className={styles.counter}>
       <h3>Update the count and edit src/App.tsx, state is preserved</h3>
       <button type="button" onClick={() => setCount((c) => c + 1)}>
         Count - {count}
       </button>
+      <h3>{name}</h3>
     </div>
   );
 };
