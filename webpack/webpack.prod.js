b32464a2eff85c6a3460806eee33c369da771f7e
MikRyam <mikhailryamov@gmail.com>
2023-02-26 10:28:18 +0300

    add MiniCssExtractPlugin

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,4 +1,19 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   mode: "production",
   devtool: 'source-map',
-}
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
